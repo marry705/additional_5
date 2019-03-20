a3e7f0dab7811d9c1a617dd3771a04cdb7a23610
Marry705 <marry705chilo@gmail.com>
2019-03-20 21:07:55 +0300

    feat:change algaritm, add stack construction

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
-module.exports = function check(str = '', bracketsConfig = []) {
-    const count = bracketsConfig.length;
-    for (let i = 0; i<count; i++){
-        let target1 = bracketsConfig[i][0];
-        let target2 = bracketsConfig[i][1];
-        let pos1 = -1;
-        let pos2 = -1;
-        while ( (pos1 = str.indexOf(target1, pos1+1) )!=-1) {
-           while ( (pos2 = str.indexOf(target2, pos2+1) )!=-1 ) {
-                if ( pos1<pos2 && (pos2-pos1)%2===1){ 
-                    str=str.substr(0,pos1)+str.substr(pos1+1,pos2-pos1-1)+str.substr(pos2+1,str.length);
-                    pos1=-1;
-                    pos2=-1;
-                    break; 
-                }  
-            if (str.length===0) break;     
-           }          
-        }      
-     }
-    return str.length === 0?true:false;
+module.exports = function check (str = '', bracketsConfig = []) {
+    if( str.length % 2 != 0 ) return false;
+    const stack = [];
+    for ( let element of str) {
+        bracketsConfig.forEach( elem => {
+            let target1 = elem[0];
+            let target2 = elem[1];
+            let last = stack[stack.length-1];
+            if ( element === target2 && stack.length != 0 && last === target1 ) stack.pop(); 
+            else{
+                if ( element === target1 ) stack.push(element);
+            }
+        });
+    } 
+    return stack.length ? false : true;
 }
 
 
